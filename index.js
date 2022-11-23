2c9b2082cbcb27bd04714c99a98bcdf5570529e8
Yuri Padilla <yuripadilla017@gmail.com>
2022-11-23 21:53:21 +0100

    add dark modus to profile

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+const bodyElement = document.querySelector('[data-js="body--dark"]');
+const questionsSymbolElement = document.querySelector('[data-js="questions--symbol--dark"]');
+const imgElement = document.querySelector('[data-js="bookmark--darkmodus"]');
+
+
+const buttonToggle = document.querySelector('[data-js="button--toggle"]');
+buttonToggle.addEventListener("click", () => {
+    bodyElement.classList.toggle("darkModus")
+    questionsSymbolElement.classList.toggle("counter__questions--symbol--dark");
+    //imgElement.src = "./icons/icon-bookmark.png";
+});
