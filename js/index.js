bf82d7dc719a3d128b7e5821229ee3d0376f07a7
Yuri Padilla <yuripadilla017@gmail.com>
2023-01-02 23:28:37 +0100

    add filter for bookmarked

--- js/index.js
+++ js/index.js
@@ -4,123 +4,153 @@ const cards = [
         tags: ["#html", "#flexbox", "#css"], 
         answer: "The Property is 'html'",
         isBookmarked: false,
-        showAnswer: false,
+        answerShowed: false,
     },
     {
         question: "What property flips the axes in Flexbox?", 
         tags: ["#html", "#flexbox", "#css"], 
         answer: "The Property is 'html'",
         isBookmarked: false,
-        showAnswer: false,
+        answerShowed: false,
     },
     {
         question: "What property flips the axes in Flexbox?", 
         tags: ["#html", "#flexbox", "#css"], 
         answer: "The Property is 'html'",
         isBookmarked: false,
-        showAnswer: false,
+        answerShowed: false,
+    },
+    {
+        question: "What property flips the axes in Flexbox?", 
+        tags: ["#html", "#flexbox", "#css"], 
+        answer: "The Property is 'html'",
+        isBookmarked: false,
+        answerShowed: false,
     },
 ];
 
-cards.forEach((card) => {
-    document.body.innerHTML += `
-        <section class="card">
-            <img class="bookmark-icon" src="./icons/icon-bookmark.svg" alt="bookmark" data-js="bookmark1" />
+const mainContainer = document.querySelector('[data-js="main-container"]');
 
-            <h2>${card.question}</h2>
+let cardsToDisplay = "all";
+function toggleCardsToDisplay(newValue) {
+    cardsToDisplay = newValue;
+}
 
-            <ul class="tag-list">
-                ${card.tags.map((tag) => {
-                    return `<li class="tag-list__item">${tag}</li>`;
-                }).join("")}
-            </ul>
-            
-            <button class="button" data-js="button-answer1">${card.showAnswer === false ? "Show Answer" : "Hide Answer"}</button>
-
-            <div class="output">
-                <output data-js="showed-answer1"></output>
-            </div>
-        </section>
-    `;
+const iconToShowHome = document.querySelector('[data-js="icon-home"]');
+iconToShowHome.addEventListener("click", () => {
+    mainContainer.innerHTML = "";
+    toggleCardsToDisplay("all");
+    displayCards();
 });
 
-const bookmark1Element = document.querySelector('[data-js="bookmark1"]');
-const bookmarkButton1 = document.querySelector('[data-js="bookmark__button1"]');
-bookmarkButton1.addEventListener("click", () => {
-    if(bookmark1Element.classList.contains("marked")) {
-        bookmark1Element.src = "./icons/icon-bookmark.png";
-        bookmark1Element.classList.toggle("marked");
-    } else {
-        bookmark1Element.src = "./icons/icon-bookmark-filled.png";
-        bookmark1Element.classList.toggle("marked");
-    }
+const iconToShowBookmark = document.querySelector('[data-js="icon-bookmark"]');
+iconToShowBookmark.addEventListener("click", () => {
+    mainContainer.innerHTML = "";
+    toggleCardsToDisplay("bookmarked");
+    displayCards();
 });
 
-const buttonShowHide1 = document.querySelector('[data-js="button-anwer1"]');
-const showedAnswer1 = document.querySelector('[data-js="showed-answer1"]');
-buttonShowHide1.addEventListener("click", () => {
-    if (buttonShowHide1.textContent === "Show Answer") {
-        buttonShowHide1.textContent = "Hide Answer";
-        showedAnswer1.textContent = "The Property is 'html'";
-    } else if (buttonShowHide1.textContent === "Hide Answer") {
-        buttonShowHide1.textContent = "Show Answer";
-        showedAnswer1.textContent = "";
+displayCards();
+
+function displayCards() {
+    if (cardsToDisplay === "all") {
+        showAllCards();
+    } else if (cardsToDisplay === "bookmarked") {
+        showBookmarkedCards();
     }
+}
+
+function showAllCards() {
+    cards.forEach((card) => {
+
+        const sectionCard = document.createElement("section");
+        sectionCard.classList.add("card");
     
-});
-
-/*++++++++++++++++++++*/
-
-const bookmark2Element = document.querySelector('[data-js="bookmark2"]');
-const bookmarkButton2 = document.querySelector('[data-js="bookmark__button2"]');
-bookmarkButton2.addEventListener("click", () => {
-    if(bookmark2Element.classList.contains("marked")) {
-        bookmark2Element.src = "./icons/icon-bookmark.png";
-        bookmark2Element.classList.toggle("marked");
-    } else {
-        bookmark2Element.src = "./icons/icon-bookmark-filled.png";
-        bookmark2Element.classList.toggle("marked");
-    }
-});
-
-const buttonShowHide2 = document.querySelector('[data-js="button-anwer2"]');
-const showedAnswer2 = document.querySelector('[data-js="showed-answer2"]');
-buttonShowHide2.addEventListener("click", () => {
-    if (buttonShowHide2.textContent === "Show Answer") {
-        buttonShowHide2.textContent = "Hide Answer";
-        showedAnswer2.textContent = "The Property is 'html'";
-    } else if (buttonShowHide2.textContent === "Hide Answer") {
-        buttonShowHide2.textContent = "Show Answer";
-        showedAnswer2.textContent = "";
-    }
+        const imgBookmark = document.createElement("img");
+        imgBookmark.classList.add("bookmark-icon");
+        imgBookmark.setAttribute("src", `./icons/${card.isBookmarked === false ? "icon-bookmark.svg" : "icon-bookmark-filled.svg"}`);
+        imgBookmark.addEventListener("click", () => {
+            card.isBookmarked = !card.isBookmarked;
+            imgBookmark.setAttribute("src", `./icons/${card.isBookmarked === false ? "icon-bookmark.svg" : "icon-bookmark-filled.svg"}`);
+        });
+        imgBookmark.setAttribute("alt", "bookmark");
     
-});
-
-/*++++++++++++++++++++*/
-
-const bookmark3Element = document.querySelector('[data-js="bookmark3"]');
-const bookmarkButton3 = document.querySelector('[data-js="bookmark__button3"]');
-bookmarkButton3.addEventListener("click", () => {
-    if(bookmark3Element.classList.contains("marked")) {
-        bookmark3Element.src = "./icons/icon-bookmark.png";
-        bookmark3Element.classList.toggle("marked");
-    } else {
-        bookmark3Element.src = "./icons/icon-bookmark-filled.png";
-        bookmark3Element.classList.toggle("marked");
-    }
-});
-
-const buttonShowHide3 = document.querySelector('[data-js="button-anwer3"]');
-const showedAnswer3 = document.querySelector('[data-js="showed-answer3"]');
-buttonShowHide3.addEventListener("click", () => {
-    if (buttonShowHide3.textContent === "Show Answer") {
-        buttonShowHide3.textContent = "Hide Answer";
-        showedAnswer3.textContent = "The Property is 'html'";
-    } else if (buttonShowHide3.textContent === "Hide Answer") {
-        buttonShowHide3.textContent = "Show Answer";
-        showedAnswer3.textContent = "";
-    }
+        const h2Question = document.createElement("h2");
+        h2Question.textContent = card.question;
     
-});
+        const ulTagsContainer = document.createElement("ul");
+        ulTagsContainer.classList.add("tag-list");
+        card.tags.forEach((tag) => {
+            const newTag = document.createElement("li");
+            newTag.classList.add("tag-list__item");
+            newTag.textContent = tag;
+            ulTagsContainer.append(newTag);
+        });
+    
+        const buttonShowHideAnswer = document.createElement("button");
+        buttonShowHideAnswer.classList.add("button");
+        buttonShowHideAnswer.setAttribute("type", "button");
+        buttonShowHideAnswer.textContent = (card.answerShowed === false ? "Show Answer" : "Hide Answer");
+        buttonShowHideAnswer.addEventListener("click", () => {
+            card.answerShowed = !card.answerShowed;
+            buttonShowHideAnswer.textContent = (card.answerShowed === false ? "Show Answer" : "Hide Answer");
+            outputAnswer.textContent = (card.answerShowed === false ? "" : card.answer);
+        });
+    
+        const outputAnswer = document.createElement("output");
+        outputAnswer.classList.add("output");
+        outputAnswer.textContent = (card.answerShowed === false ? "" : card.answer);
+    
+        sectionCard.append(imgBookmark, h2Question, ulTagsContainer, buttonShowHideAnswer, outputAnswer);
+    
+        mainContainer.append(sectionCard);
+    });
+}
 
-/*++++++++++++++++++++*/
+function showBookmarkedCards() {
+    cards.forEach((card) => {
+        if (card.isBookmarked === true) {
+            const sectionCard = document.createElement("section");
+        sectionCard.classList.add("card");
+    
+        const imgBookmark = document.createElement("img");
+        imgBookmark.classList.add("bookmark-icon");
+        imgBookmark.setAttribute("src", `./icons/${card.isBookmarked === false ? "icon-bookmark.svg" : "icon-bookmark-filled.svg"}`);
+        imgBookmark.addEventListener("click", () => {
+            card.isBookmarked = !card.isBookmarked;
+            imgBookmark.setAttribute("src", `./icons/${card.isBookmarked === false ? "icon-bookmark.svg" : "icon-bookmark-filled.svg"}`);
+        });
+        imgBookmark.setAttribute("alt", "bookmark");
+    
+        const h2Question = document.createElement("h2");
+        h2Question.textContent = card.question;
+    
+        const ulTagsContainer = document.createElement("ul");
+        ulTagsContainer.classList.add("tag-list");
+        card.tags.forEach((tag) => {
+            const newTag = document.createElement("li");
+            newTag.classList.add("tag-list__item");
+            newTag.textContent = tag;
+            ulTagsContainer.append(newTag);
+        });
+    
+        const buttonShowHideAnswer = document.createElement("button");
+        buttonShowHideAnswer.classList.add("button");
+        buttonShowHideAnswer.setAttribute("type", "button");
+        buttonShowHideAnswer.textContent = (card.answerShowed === false ? "Show Answer" : "Hide Answer");
+        buttonShowHideAnswer.addEventListener("click", () => {
+            card.answerShowed = !card.answerShowed;
+            buttonShowHideAnswer.textContent = (card.answerShowed === false ? "Show Answer" : "Hide Answer");
+            outputAnswer.textContent = (card.answerShowed === false ? "" : card.answer);
+        });
+    
+        const outputAnswer = document.createElement("output");
+        outputAnswer.classList.add("output");
+        outputAnswer.textContent = (card.answerShowed === false ? "" : card.answer);
+    
+        sectionCard.append(imgBookmark, h2Question, ulTagsContainer, buttonShowHideAnswer, outputAnswer);
+    
+        mainContainer.append(sectionCard);
+        }
+    });
+}
